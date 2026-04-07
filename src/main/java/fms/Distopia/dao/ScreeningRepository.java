eb9751f7f5749c3590623bd42a1e134b9c0483f6
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-07 14:44:15 +0200

    feat(screening): create ScreeningRepository interface

---
+++ src/main/java/fms/Distopia/dao/ScreeningRepository.java
@@ -0,0 +1,9 @@
+package fms.Distopia.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fms.Distopia.entities.Screening;
+
+public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+
+}
