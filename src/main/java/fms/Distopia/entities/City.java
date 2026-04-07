11cd03120efb97a9f7ec6c6dd27a1fe64376af00
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-07 11:05:02 +0200

    feat(city): add City entity

---
+++ src/main/java/fms/Distopia/entities/City.java
@@ -0,0 +1,24 @@
+package fms.Distopia.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class City implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String zipcode;
+
+}
