755bf6640c5fa446343ffa028672a366932813e6
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-07 12:58:35 +0200

    feat(movie): add Movie entity with Screening relationships

---
+++ src/main/java/fms/Distopia/entities/Movie.java
@@ -0,0 +1,31 @@
+package fms.Distopia.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Movie implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private int duration;
+    private String genre;
+
+    @OneToMany(mappedBy = "movie")
+    private Collection<Screening> screenings;
+
+}
