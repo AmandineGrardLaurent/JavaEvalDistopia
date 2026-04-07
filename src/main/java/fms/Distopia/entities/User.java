c44b408710e48192850a4eda78effab864b7ea05
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-07 10:57:22 +0200

    feat(user): add User entity with Role enum

---
+++ src/main/java/fms/Distopia/entities/User.java
@@ -0,0 +1,30 @@
+package fms.Distopia.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String lastname;
+    private String firstname;
+    private String email;
+    private String password;
+}
