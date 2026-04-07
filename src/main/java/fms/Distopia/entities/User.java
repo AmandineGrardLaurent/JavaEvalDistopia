e4ab505b39966113030627647f1194763b2c6940
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-07 12:59:18 +0200

    feat(reservation): add Reservation entity with Screening and User relationships

--- src/main/java/fms/Distopia/entities/User.java
+++ src/main/java/fms/Distopia/entities/User.java
@@ -1,6 +1,7 @@
 package fms.Distopia.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,6 +9,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -27,4 +29,7 @@ public class User implements Serializable {
     private String firstname;
     private String email;
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    private Collection<Reservation> reservations;
 }
