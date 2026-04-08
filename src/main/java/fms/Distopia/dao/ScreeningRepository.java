138f485dc3779a5a24eb5916cc89e4e347c16ce8
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-08 10:37:00 +0200

    feat(screening): add findByCinemaId method in ScreeningRepository

--- src/main/java/fms/Distopia/dao/ScreeningRepository.java
+++ src/main/java/fms/Distopia/dao/ScreeningRepository.java
@@ -1,9 +1,12 @@
 package fms.Distopia.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fms.Distopia.entities.Screening;
 
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+    public List<Screening> findByCinemaId(Long cinemaId);
 
 }
