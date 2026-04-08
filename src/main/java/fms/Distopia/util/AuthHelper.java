769a860743a3c06f3891bf77fdb673a9413796bd
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-08 14:27:21 +0200

    feat(auth): add session-based authentication helpers (getUser, isAdmin)

--- src/main/java/fms/Distopia/util/AuthHelper.java
+++ src/main/java/fms/Distopia/util/AuthHelper.java
@@ -2,8 +2,27 @@ package fms.Distopia.util;
 
 import javax.servlet.http.HttpSession;
 
+import fms.Distopia.entities.Role;
+import fms.Distopia.entities.User;
+
 public class AuthHelper {
+
+    public static User getUser(HttpSession session) {
+        Object obj = session.getAttribute("user");
+
+        if (obj instanceof User) {
+            return (User) obj;
+        }
+
+        return null;
+    }
+
     public static boolean isLogged(HttpSession session) {
-        return session.getAttribute("user") != null;
+        return getUser(session) != null;
+    }
+
+    public static boolean isAdmin(HttpSession session) {
+        User user = getUser(session);
+        return user != null && user.getRole() == Role.ADMIN;
     }
 }
