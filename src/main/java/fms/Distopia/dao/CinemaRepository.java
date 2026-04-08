71a1abda875f7d541a16997ad0bd4066d50e7958
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-08 08:41:58 +0200

    feat(cinema): add findByCityName query method in CinemaRepository

--- src/main/java/fms/Distopia/dao/CinemaRepository.java
+++ src/main/java/fms/Distopia/dao/CinemaRepository.java
@@ -1,9 +1,12 @@
 package fms.Distopia.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fms.Distopia.entities.Cinema;
 
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
+    public Page<Cinema> findByCityName(String cityName, Pageable pageable);
 }
