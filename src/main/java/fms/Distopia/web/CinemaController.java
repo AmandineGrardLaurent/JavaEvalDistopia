37d230008369d9175b6e3e7ef1df04df073aeaa6
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-08 08:42:22 +0200

    feat(cinema): implement filters in view and controller

--- src/main/java/fms/Distopia/web/CinemaController.java
+++ src/main/java/fms/Distopia/web/CinemaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fms.Distopia.dao.CinemaRepository;
+import fms.Distopia.dao.CityRepository;
 import fms.Distopia.entities.Cinema;
 
 @Controller
@@ -17,16 +18,33 @@ public class CinemaController {
     @Autowired
     CinemaRepository cinemaRepository;
 
+    @Autowired
+    CityRepository cityRepository;
+
+    /**
+     * Displays the list of cinemas with optional pagination and city name-based
+     * filtering.
+     * 
+     * @return the "cinema/list" view template
+     */
     @GetMapping("/cinema/list")
     public String read(Model model,
-            @RequestParam(name = "page", defaultValue = "0") int page) {
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "cityName", required = false) String cityName) {
 
         Page<Cinema> cinemas;
 
-        cinemas = cinemaRepository.findAll(PageRequest.of(page, 6));
+        if (cityName != null && !cityName.isEmpty()) {
+            cinemas = cinemaRepository.findByCityName(cityName, PageRequest.of(page, 6));
+        } else {
+            cinemas = cinemaRepository.findAll(PageRequest.of(page, 6));
+        }
+
         model.addAttribute("cinemas", cinemas);
         model.addAttribute("pages", new int[cinemas.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("cityName", cityName);
+        model.addAttribute("cities", cityRepository.findAll());
 
         return "cinema/list";
     }
