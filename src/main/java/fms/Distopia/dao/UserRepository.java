5d267fb065a0884b3ece218a0cd8d13e4b75eaa0
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-08 13:22:03 +0200

    feat(auth): add findByEmail method in UserRepository

--- src/main/java/fms/Distopia/dao/UserRepository.java
+++ src/main/java/fms/Distopia/dao/UserRepository.java
@@ -1,9 +1,11 @@
 package fms.Distopia.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fms.Distopia.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    public Optional<User> findByEmail(String email);
 }
