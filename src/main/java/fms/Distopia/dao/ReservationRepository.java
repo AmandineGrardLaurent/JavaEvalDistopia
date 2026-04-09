934b610cc260b821490b8f550056ca799e712107
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-09 15:03:59 +0200

    feat(reservation): add findByUserId method in ReservationRepository

--- src/main/java/fms/Distopia/dao/ReservationRepository.java
+++ src/main/java/fms/Distopia/dao/ReservationRepository.java
@@ -1,9 +1,12 @@
 package fms.Distopia.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fms.Distopia.entities.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    public List<Reservation> findByUserId(Long userId);
 }
